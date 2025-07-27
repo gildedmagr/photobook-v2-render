476b508e9249def1f6eea44171a7090fb18df2f1
andrii <alltelex@gmail.com>
2025-07-27 14:12:46 +0300

    Add `@socket.io/redis-adapter` dependencies for Redis integration

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const app = express();
 const http = require('http').createServer(app);
 const route = require('./src/routes');
 const {socketService} = require('./src/services');
-const path = require("path");
+const { createAdapter } = require('@socket.io/redis-adapter');
+const { createClient } = require('redis');
 
 const io = socketService.createSocketInstance(http);
+const pubClient = createClient({ url: 'redis://redis:6379' });
+const subClient = pubClient.duplicate();
+
+await pubClient.connect();
+await subClient.connect();
+io.adapter(createAdapter(pubClient, subClient));
 
 const port = process.env.PORT || 3000;
 
